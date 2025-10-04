29922395c68f038966f5bb6407f9ec8436d11af0
AlLCWGithub <alson20110204@gmail.com>
2025-10-04 22:08:32 +0800

    Updated ModItems.java

--- src/main/java/net/alsonminecraft/gemstones/item/ModItems.java
+++ src/main/java/net/alsonminecraft/gemstones/item/ModItems.java
@@ -6,10 +6,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-	public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+	public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Gemstones.MOD_ID, "ruby")))));
 
 	private static Item registerItem(String name, Item item) {
 		return Registry.register(Registries.ITEM, Identifier.of(Gemstones.MOD_ID, name), item);
