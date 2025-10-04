c58d756e2ab02784cc4d1809baeca39ba773f432
AlLCWGithub <alson20110204@gmail.com>
2025-10-04 19:04:37 +0800

    Added GemstoneClient.java and Modified Gemstone.java and fabric.mod.json

---
+++ src/main/java/net/alsonminecraft/gemstones/GemstonesClient.java
@@ -0,0 +1,10 @@
+package net.alsonminecraft.gemstones;
+
+import net.fabricmc.api.ClientModInitializer
+
+public class GemstonesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
