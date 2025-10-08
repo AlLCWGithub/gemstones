161e5f5e334901e5b53c139617ef04de8a835577
AlLCWGithub <alson20110204@gmail.com>
2025-10-08 21:03:57 +0800

    Updated repository

--- src/main/java/net/alsonminecraft/gemstones/Gemstones.java
+++ src/main/java/net/alsonminecraft/gemstones/Gemstones.java
@@ -17,6 +17,6 @@ public class Gemstones implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-		ModItemGroups.registerModItemGroups();
+		ModItemGroups.registerItemGroups();
 	}
 }
--- src/main/java/net/alsonminecraft/gemstones/item/ModItemGroups.java
+++ src/main/java/net/alsonminecraft/gemstones/item/ModItemGroups.java
@@ -1,8 +1,11 @@
 package net.alsonminecraft.gemstones.item;
 
 import net.alsonminecraft.gemstones.Gemstones;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registry;
 
 public class ModItemGroups {
+	public static final ItemGroup GEMSTONES_ITEMS = Registry.registry
 
 	public static void registerItemGroups() {
 		Gemstones.LOGGER.info("Registering Item Groups for " + Gemstones.MOD_ID);
